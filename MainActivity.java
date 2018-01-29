8b8f6ef293c3059ce2f7f89415b9e97c8ca150de
senweiliang <767098234@qq.com>
2018-01-29 20:44:26 +0800

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,7 @@
 package com.senweiliang.habbits;
 
 import android.content.pm.ActivityInfo;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,7 @@ import android.view.ViewConfiguration;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.senweiliang.habbits.adapter.MyDataAdapter;
 import com.senweiliang.habbits.repository.DataRepository;
@@ -22,23 +24,34 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.main_listview) ListView listView;
+    @BindView(R.id.main_listview) ListView  listView;
     private DataRepository repository=null;
+    SQLiteDatabase sqLiteDatabase=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//保持竖屏
+        listView.setEmptyView(findViewById(R.id.empty_layout));
         MyDataAdapter adapter=new MyDataAdapter(this, DataRepository.data);
         listView.setAdapter(adapter);
+        sqLiteDatabase=this.openOrCreateDatabase("habit_app.db",MODE_PRIVATE,null);
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS signInData(habit_name Text,ID INTEGER)");
 
+    }
+    @OnClick
+    public void AddHabit(MenuItem item){
 
+        sqLiteDatabase.execSQL("INSERT INTO signInData VALUES('Testing',1)");
+        Toast.makeText(this, "SQLite AddHabit", Toast.LENGTH_SHORT).show();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater=getMenuInflater();
         inflater.inflate(R.menu.main_menu,menu);
+
+
         return true;
     }
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -46,16 +59,25 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_about) {
+            return true;
+        }
+        if (id==R.id.action_add){
+
+            AddHabit(item);
+        }
+        if (id==R.id.action_calendar){
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        sqLiteDatabase.close();
+    }
 
-//    @OnClick(R.id.button)
-//    public void change(View view){
-//        textView.setText("你好世界！");
-//    }
 
 }
